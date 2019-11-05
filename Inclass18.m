6fca490d99d43606686d7a57847eff0138105e27
warmflasha <warmflasha@gmail.com>
2019-11-05 11:18:56 -0600

    Added inclass188

---
+++ Inclass18.m
@@ -0,0 +1,4 @@
+%In this folder, you will find two images img1.tif and img2.tif that have
+%some overlap. Use two different methods to align them - the first based on
+%pixel values in the original images and the second using the fourier
+%transform of the images. In both cases, display your results. 
